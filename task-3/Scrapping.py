e811b23462b0f714c4ef6bf8ad220b9be126f2a0
Paayas P <49279142+PAAYAS@users.noreply.github.com>
2021-03-23 12:31:25 +0530

    Add files via upload

---
+++ task-3/Scrapping.py
@@ -0,0 +1,16 @@
+import requests
+url =' https://www.worldometers.info/coronavirus'
+result = requests.get(url)
+import bs4
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+cases = soup.find_all('div', class_ = 'maincounter-number')
+data = []
+for i in cases:
+    span = i .find('span')
+    data.append(span.string)
+import pandas as pd
+df = pd.DataFrame({'CoronaData': data})
+df.index = ['Total cases', 'Total deaths', 'Total recovered']
+df
+df.to_csv('Corona_Data.csv')
+
